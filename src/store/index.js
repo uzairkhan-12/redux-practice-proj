c70ae6869b89e86c00345da0d826baf3a113460a
uzairkhan-12 <pk.uzikhan@gmail.com>
2023-03-27 09:42:49 +0500

    initial commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,62 @@
-import { createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const reducerFn = (state = {counter:0},action) => {
-//this should be a synchronous function
-//we should not mute the original state
+const counterSlice = createSlice({
+    name:'counter',
+    initialState: {counter:0},
+    reducers: {
+        increment(state,action){
+             state.counter ++
+        },
+        decrement(state,action){
+             state.counter --
+        },
+        addBy(state,action){
+             state.counter += action.payload
+        }
+    }
+})
+export const actions = counterSlice.actions
+const store = configureStore({
+    reducer: counterSlice.reducer})
+    export default store
 
-if(action.type === 'INC'){
-    return {counter: state.counter+1}
-}
-if(action.type === 'DEC'){
-    return {counter:state.counter-1}
-}
-if(action.type === 'ADD'){
-    return {counter:state.counter + action.payload}
-}
-return state
-}
-const store = createStore(reducerFn)
-export default store
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { createStore } from "redux";
+
+// const reducerFn = (state = {counter:0},action) => {
+// //this should be a synchronous function
+// //we should not mute the original state
+
+// if(action.type === 'INC'){
+//     return {counter: state.counter+1}
+// }
+// if(action.type === 'DEC'){
+//     return {counter:state.counter-1}
+// }
+// if(action.type === 'ADD'){
+//     return {counter:state.counter + action.payload}
+// }
+// return state
+// }
+// const store = createStore(reducerFn)
+// export default store
